8ed754abdf0dc339534aa0b8fe1763d8013581c1
Krunal Vaghela <krunalvaghela2008@gmail.com>
2025-07-20 13:57:22 +0530

    changes

--- 20-07-2025 - lecture number 1/firstProgram.cpp
+++ 20-07-2025 - lecture number 1/firstProgram.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main(){
-    std::cout << "Hii, My name is Krunal Vaghela ";
+    std::cout << "Hii, My name is Krunal Vaghela and Iim working on lecture number 1. ";
 
     return 0;
 }
